f8c771809e92c329e600729d8c0249173bd31854
Jakub Malkowski <malkowski2003@gmail.com>
2025-05-18 14:10:37 +0200

    start add crud for buildertool

---
+++ BuilderTools/src/BuilderTools.Api/Controllers/BuilderToolController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BuilderTools.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BuilderToolController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BuilderTools/src/BuilderTools.Core/DTO/BuilderToolInputDto.cs
@@ -0,0 +1,29 @@
+﻿
+using BuilderTools.Core.Model;
+
+namespace BuilderTools.Core.DTO
+{
+    public class BuilderToolInputDto
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public bool Permission { get; set; } = false;
+        public decimal PricePerDay { get; set; }
+        public Byte[] Image { get; set; }
+
+        public BuilderToolInputDto() { }
+        public BuilderToolInputDto(Guid categoryId, string name, bool permission, decimal pricePerDay, byte[] image)
+        {
+            CategoryId = categoryId;
+            Name = name;
+            Permission = permission;
+            PricePerDay = pricePerDay;
+            Image = image;
+        }
+
+        public BuilderTool ToModel()
+        {
+            return new BuilderTool(Guid.NewGuid(), CategoryId, Name, Permission, PricePerDay, Image);
+        }
+    }
+}
